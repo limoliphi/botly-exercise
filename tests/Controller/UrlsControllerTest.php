f156fe7dc77926c0e9f77a936722881bd0cab370
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2020-11-18 08:18:24 +0100

    Add better tests for create action

--- tests/Controller/UrlsControllerTest.php
+++ tests/Controller/UrlsControllerTest.php
@@ -49,6 +49,34 @@ class UrlsControllerTest extends WebTestCase
     }
 
     /** @test */
+    public function create_should_shorten_url_once()
+    {
+        $client = static::createClient();
+
+        $em = self::$container->get('doctrine')->getManager();
+
+        $url = new Url();
+        $url->setOriginal('https://symfony.com');
+        $url->setShortened('qwerty');
+        $em->persist($url);
+        $em->flush();
+
+        $crawler = $client->request('GET', '/');
+
+        $form = $crawler->filter('form')->form();
+
+        $client->submit($form, [
+            'form[original]' => 'https://symfony.com',
+        ]);
+
+        $this->assertResponseRedirects('/qwerty/preview');
+
+        $urlRepository = $em->getRepository(Url::class);
+
+        $this->assertCount(1, $urlRepository->findAll());
+    }
+
+    /** @test */
     public function shortened_version_should_redirect_to_original_url_if_shortened_exists()
     {
         $client = static::createClient();
